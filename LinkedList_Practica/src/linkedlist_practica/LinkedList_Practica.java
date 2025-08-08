0981ae537d5b3d8422e372e46117c634dfa5f2f4
SerbinoTrigueros <chris.serbino@gmail.com>
2025-08-07 22:20:59 -0600

    uso de Maps

--- LinkedList_Practica/src/linkedlist_practica/LinkedList_Practica.java
+++ LinkedList_Practica/src/linkedlist_practica/LinkedList_Practica.java
@@ -5,8 +5,10 @@
 package linkedlist_practica;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -21,10 +23,25 @@ public class LinkedList_Practica {
         //se crea la lista de tipo linkedlist
        ArrayList<String> listaNumeros = new ArrayList<>();
        LinkedList<String> listaNombres = new LinkedList<>();
+       //uso de map
+       Map<String, Integer> maps = new HashMap<>();
+       maps.put("christian, edad ", 23);
+       maps.put("gabriela, edad ", 15);
+       
+        System.out.println(maps);
+
+        //uso de metodo .of
+       var listasEnlazadas = List.of(2,3,4,5,6,7);
+       listasEnlazadas.forEach(System.out::println);
+       
+        System.out.println("");
        
        var listaLetras = List.of("A","B","C");
+       
        listaLetras.forEach(System.out::println);
        
+       
+      /* 
        //se va agregar datos a la lista 
        listaNombres.add("Christian");
        listaNombres.add("Serbino");
@@ -58,6 +75,7 @@ public class LinkedList_Practica {
        
         
         }
+*/
         
     }
     
