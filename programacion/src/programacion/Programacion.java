74bbf11d5121c920fef98b4b22e393a672679c13
SerbinoTrigueros <chris.serbino@gmail.com>
2025-08-07 22:41:41 -0600

    subi mi primer practica

--- programacion/src/programacion/Programacion.java
+++ programacion/src/programacion/Programacion.java
@@ -14,7 +14,7 @@ public class Programacion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println(" mi primera practica usando repositorio :D ");
     }
     
 }
